79a5c4f7bba73be7029860885c2a6b37cd9af769
Gabriel Falcão <gabriel@nacaolivre.org>
2020-03-21 22:44:03 +0100

    poc

--- index.js
+++ index.js
@@ -11,28 +11,38 @@ function get_array_input(name) {
 }
 
 
+
 try {
   // Read inputs
-  const defaultVersion = core.getInput("default");
-  const rootName = core.getInput("pyenv_root_name") || '.pyenv';
+  const default_version = core.getInput("default");
 
   const commands = get_array_input("commands");
   const versions = get_array_input("versions");
 
-  const pyenvRoot = `${process.env.HOME}/${rootName}`
-  const newPathEnvVar = `${pyenvRoot}:${process.env.PATH}`
+  // Prepare contextual variables and urls
+  const pyenv_version = 'v1.2.17'
+  const pyenv_root_name = `.pyenv@${pyenv_version}`
+  const pyenv_tarball_url = `https://github.com/pyenv/pyenv/archive/${pyenv_version}.tar.gz`;
 
-  // Download pyenv-installer
-  const pyenvInstallerPath = await tc.downloadTool('https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer');
+  // Download pyenv tarball IO#1
+  console.log(`Downloading pyenv ${pyenv_version}...`);
+  const pyenv_tarball_path = await tc.downloadTool(pyenv_tarball_url);
 
-  // Install Pyenv
-  console.log(`Installing Pyenv ...`);
-  console.log(`pyenv global ${defaultVersion}...`);
+  // Extract tarball
+  console.log(`Installing pyenv ${pyenv_version}...`);
+  const pyenv_root_path = await tc.extractZip(pyenv_tarball_path, pyenv_root_name);
 
-  // Install
-  core.exportVariable('PATH', newPath);
-  core.exportVariable('PYENV_SHELL', newPath);
-  core.setOutput("pyenv_root", pyenvRoot);
+  // Cache for future usage
+  const cached_pyenv_path = await tc.cacheDir(pyenv_root_path, 'pyenv', pyenv_version);
+  console.log(`pyenv local ${default_version}...`);
+
+  // Safely determine the location of pyenv shims
+  const pyenv_shims_path = `${cached_pyenv_path}/shims`
+
+  // Prepend shims location to PATH
+  core.addPath(pyenv_shims_path);
+  core.exportVariable('PYENV_SHELL', process.env.SHELL);
+  core.setOutput("pyenv_root", cached_pyenv_path);
 
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`Event payload: ${payload}`);
